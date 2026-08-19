f0f8c637fceb2415a656d50d82069e456dd41b91
gokulslabs <gokulslabs@users.noreply.github.com>
2026-08-19 13:31:02 +0530

    Add brand SVG favicon (SG monogram)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -106,6 +106,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
       { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
     ],
   }),
